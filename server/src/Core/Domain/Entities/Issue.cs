59869848783ae69b1e720ed3a6360b0fd1713358
SAW <99191294+nicsaw@users.noreply.github.com>
2025-06-23 23:47:24 +1000

    fix: require more Issue attributes

--- server/src/Core/Domain/Entities/Issue.cs
+++ server/src/Core/Domain/Entities/Issue.cs
@@ -3,20 +3,20 @@ using Domain.Entities.Enums;
 
 public class Issue : BaseEntity
 {
-    public int VehicleID { get; set; }
-    public int IssueNumber { get; set; }
-    public int ReportedBy { get; set; }
+    public required int VehicleID { get; set; }
+    public required int IssueNumber { get; set; }
+    public required int ReportedBy { get; set; }
     public required string Title { get; set; }
     public string? Description { get; set; }
-    public IssueCategoryEnum Category { get; set; }
-    public PriorityLevelEnum PriorityLevel { get; set; }
-    public IssueStatusEnum Status { get; set; }
+    public required IssueCategoryEnum Category { get; set; }
+    public required PriorityLevelEnum PriorityLevel { get; set; }
+    public required IssueStatusEnum Status { get; set; }
     public DateTime? ResolvedDate { get; set; }
-    public int ResolvedBy { get; set; }
+    public int? ResolvedBy { get; set; }
     public string? ResolutionNotes { get; set; }
 
     // Navigation Properties
     public required ICollection<IssueAttachment> IssueAttachment { get; set; }
     public required ICollection<WorkOrderIssue> WorkOrderIssue { get; set; }
-    // TODO: Connects to InspectionAttachment, User
+    // TODO: Connect to Vehicle, User
 }
