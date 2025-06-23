cdeae3fdce2cd2162eb2939b30eb7b70cad36542
SAW <99191294+nicsaw@users.noreply.github.com>
2025-06-23 23:06:16 +1000

    feat: Issue properties

---
+++ server/src/Core/Domain/Entities/Issue.cs
@@ -0,0 +1,20 @@
+namespace Domain.Entities;
+using Domain.Entities.Enums;
+
+public class Issue : BaseEntity
+{
+    public int VehicleID { get; set; }
+    public int IssueNumber { get; set; }
+    public int ReportedBy { get; set; }
+    public required string Title { get; set; }
+    public string? Description { get; set; }
+    public IssueCategoryEnum Category { get; set; }
+    public PriorityLevelEnum PriorityLevel { get; set; }
+    public IssueStatusEnum Status { get; set; }
+    public DateTime? ResolvedDate { get; set; }
+    public int ResolvedBy { get; set; }
+    public string? ResolutionNotes { get; set; }
+
+    // Navigation Properties
+    
+}
