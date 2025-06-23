be7c332bfac6b79085153c682318c2e1208c0a0b
Clarence Muljadi <clarencemuljadi@gmail.com>
2025-06-23 23:20:45 +1000

    fix: added true as default value for IsActive in VehicleGroup

--- server/src/Core/Domain/Entities/VehicleGroup.cs
+++ server/src/Core/Domain/Entities/VehicleGroup.cs
@@ -6,5 +6,5 @@ public class VehicleGroup : BaseEntity
 {
     public required string Name { get; set; }
     public required string Description { get; set; }
-    public required bool IsActive { get; set; }
+    public required bool IsActive { get; set; } = true;
 }
