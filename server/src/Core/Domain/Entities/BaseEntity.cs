2b93c04b469a6dcf23dc118408ad9f9606cc7e7c
Dicko Evaldo <z5451386@ad.unsw.edu.au>
2025-06-23 21:59:01 +1000

    feat: add inventory related entities

---
+++ server/src/Core/Domain/Entities/BaseEntity.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Domain.Entities;
+
+public class BaseEntity
+{
+    public int ID { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
