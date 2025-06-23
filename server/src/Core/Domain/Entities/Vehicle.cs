376044cdc9d44b6fb6eacfec4fcf2c5415d60afa
Clarence Muljadi <clarencemuljadi@gmail.com>
2025-06-23 23:19:44 +1000

    fix: changed Vehicle Year to int

--- server/src/Core/Domain/Entities/Vehicle.cs
+++ server/src/Core/Domain/Entities/Vehicle.cs
@@ -8,7 +8,7 @@ public class Vehicle : BaseEntity
     public required string Name { get; set; }
     public required string Make { get; set; }
     public required string Model { get; set; }
-    public required string Year { get; set; }
+    public required int Year { get; set; }
     public required string VIN { get; set; }
     public required string LicensePlate { get; set; }
     public required DateTime LicensePlateExpirationDate { get; set; }
