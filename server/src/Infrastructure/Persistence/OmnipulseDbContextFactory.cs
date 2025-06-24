f366f4f8d44bffeaa1cfa5b98645ae7d250255a7
Dicko Evaldo <z5451386@ad.unsw.edu.au>
2025-06-24 14:03:30 +1000

    feat: add db context factory

---
+++ server/src/Infrastructure/Persistence/OmnipulseDbContextFactory.cs
@@ -0,0 +1,19 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Persistence.DatabaseContext;
+
+namespace Persistence;
+
+public class OmnipulseDbContextFactory : IDesignTimeDbContextFactory<OmnipulseDatabaseContext>
+{
+    public OmnipulseDatabaseContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<OmnipulseDatabaseContext>();
+        
+        // TODO: add db url
+        optionsBuilder.UseSqlServer("");
+        
+        return new OmnipulseDatabaseContext(optionsBuilder.Options);
+    }
+}
