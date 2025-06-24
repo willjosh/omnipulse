13ddeb0e0e5f482e5743c19483343f3b6c7d2141
SAW <99191294+nicsaw@users.noreply.github.com>
2025-06-24 19:53:43 +1000

    feat: VehicleAssignment entity

---
+++ server/src/Core/Domain/Entities/VehicleAssignment.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Domain.Entities;
+using Domain.Entities.Enums;
+
+public class VehicleAssignment : BaseEntity
+{
+    // FKs
+    public required int VehicleID { get; set; }
+    public required int AssignedToUserID { get; set; }
+
+    public required DateTime AssignedDate { get; set; }
+    public DateTime? UnassignedDate { get; set; }
+    public required bool IsActive { get; set; } = true;
+    public required AssignmentTypeEnum AssignmentType { get; set; }
+    public string? Notes { get; set; }
+
+    // Navigation Properties
+    public required Vehicle Vehicle { get; set; }
+    // public required User User { get; set; }
+}
