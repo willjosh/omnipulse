9e5e63c2e4f280a9bb6df28a674226c51b839c9d
SAW <99191294+nicsaw@users.noreply.github.com>
2025-06-24 19:46:23 +1000

    feat: VehicleDocument entity

---
+++ server/src/Core/Domain/Entities/VehicleDocument.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Domain.Entities;
+using Domain.Entities.Enums;
+
+public class VehicleDocument : BaseEntity
+{
+    // FKs
+    public required int VehicleID { get; set; }
+    public required int UploadedBy { get; set; }
+
+    public required DocumentTypeEnum DocumentType { get; set; }
+    public required string Title { get; set; }
+    public required string FileName { get; set; }
+    public required string FilePath { get; set; }
+    public required int FileSize { get; set; }
+    public DateTime? ExpiryDate { get; set; }
+    public string? Notes { get; set; }
+
+    // Navigation Properties
+    public required Vehicle Vehicle { get; set; }
+    // public required User User { get; set; }
+}
