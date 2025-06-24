19d28632d7a73870b7a9e5afbfd6db2aac608dbc
Clarence Muljadi <clarencemuljadi@gmail.com>
2025-06-24 21:52:02 +1000

    feat: added Invoice class

---
+++ server/src/Core/Domain/Entities/Enums/InvoiceStatusEnum.cs
@@ -0,0 +1,11 @@
+namespace Domain.Entities.Enums;
+
+public enum InvoiceStatusEnum
+{
+    DRAFT,
+    OPEN,
+    PAID,
+    CANCELLED,
+    OVERDUE,
+    VOIDED
+}
---
+++ server/src/Core/Domain/Entities/Invoice.cs
@@ -0,0 +1,22 @@
+using System;
+using Domain.Entities.Enums;
+
+namespace Domain.Entities;
+
+public class Invoice
+{
+    public required int WorkOrderID { get; set; }
+    public required int GeneratedByID { get; set; }
+    public required string InvoiceNumber { get; set; }
+    public required DateTime InvoiceDate { get; set; }
+    public required double TotalAmount { get; set; }
+    public required double TaxAmount { get; set; }
+    public required InvoiceStatusEnum Status { get; set; }
+    public required DateTime? PaymentDate { get; set; }
+    public required string PaymentMethod { get; set; }
+
+    // Navigation properties
+    public required WorkOrder WorkOrder { get; set; }
+
+    // TODO: Add navigation properties for GeneratedBy
+}
