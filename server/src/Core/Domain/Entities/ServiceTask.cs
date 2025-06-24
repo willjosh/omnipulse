c56e068ca6e305aa77987506bdcd8302686f05ff
SAW <99191294+nicsaw@users.noreply.github.com>
2025-06-24 22:27:33 +1000

    feat: ServiceTask entity

---
+++ server/src/Core/Domain/Entities/ServiceTask.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Domain.Entities;
+using Domain.Entities.Enums;
+
+public class ServiceTask : BaseEntity
+{
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+    public required double EstimatedLabourHours { get; set; }
+    public required decimal EstimatedCost { get; set; }
+    public required ServiceTaskCategoryEnum Category { get; set; }
+    public required bool IsActive { get; set; } = true;
+
+    // Navigation properties
+    // public required ICollection<ServiceScheduleTask> ServiceScheduleTasks { get; set; } = [];
+    public required ICollection<MaintenanceHistory> MaintenanceHistories { get; set; } = [];
+    public required ICollection<WorkOrderLineItem> WorkOrderLineItems { get; set; } = [];
+}
