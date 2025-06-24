2cb7ec7507a559425f6706d401813dfc4e1ffcd4
SAW <99191294+nicsaw@users.noreply.github.com>
2025-06-24 19:31:49 +1000

    feat: VehiclePhoto entity

---
+++ server/src/Core/Domain/Entities/VehiclePhoto.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Domain.Entities;
+
+public class VehicleImage : BaseEntity
+{
+    // FKs
+    public required int VehicleID { get; set; }
+    public required int UploadedBy { get; set; }
+
+    // Image
+    public required string ImageLabel { get; set; }
+    public required string FileName { get; set; }
+    public required string FilePath { get; set; }
+    public required int FileSize { get; set; } // bytes
+
+    // Navigation Properties
+    public required Vehicle Vehicle { get; set; }
+    // public required User User { get; set; }
+}
