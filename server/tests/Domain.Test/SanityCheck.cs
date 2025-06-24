155cd95c4a8b10bd5469d95a7ce93b3a5a6bbbb0
SAW <99191294+nicsaw@users.noreply.github.com>
2025-06-24 15:36:06 +1000

    feat: create dummy tests

---
+++ server/tests/Domain.Test/SanityCheck.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Test;
+
+public class SanityCheck
+{
+    [Fact]
+    public void TrueIsTrue()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void Fail()
+    {
+        Assert.True(false);
+    }
+}
