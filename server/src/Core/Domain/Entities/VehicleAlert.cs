6fdb3efa8e372ad6fcecfb9c0cc447af45f0f816
SAW <99191294+nicsaw@users.noreply.github.com>
2025-06-24 19:36:59 +1000

    feat: VehicleAlert entity

---
+++ server/src/Core/Domain/Entities/VehicleAlert.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Domain.Entities;
+using Domain.Entities.Enums;
+
+public class VehicleAlert : BaseEntity
+{
+    // FKs
+    public required int VehicleID { get; set; }
+    public required int UserID { get; set; }
+
+    public required AlertTypeEnum AlertType { get; set; }
+    public required string Title { get; set; }
+    public required string Message { get; set; }
+    public required AlertLevelEnum AlertLevel { get; set; }
+
+    public required bool IsAcknowledged { get; set; }
+    public int? AcknowledgedBy { get; set; }
+    public DateTime? AcknowledgedAt { get; set; }
+    public DateTime? ExpiresAt { get; set; }
+    public required bool IsDismissed { get; set; }
+
+    // Navigation Properties
+    public required Vehicle Vehicle { get; set; }
+    // public required User User { get; set; }
+}
