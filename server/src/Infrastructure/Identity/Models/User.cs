edb107e4eb661d0115fe478f3655673a96af4757
Dicko Evaldo <z5451386@ad.unsw.edu.au>
2025-06-25 15:46:36 +1000

    feat: set the db up for migrations

--- server/src/Infrastructure/Identity/Models/User.cs
+++ server/src/Infrastructure/Identity/Models/User.cs
@@ -5,11 +5,12 @@ namespace Identity.Models;
 
 public class User : IdentityUser
 {
-   public required string FirstName { get; set; } 
-   public required string LastName { get; set; } 
-   public required DateTime HireDate { get; set; }
-   public required Boolean IsActive { get; set; } = true;
-   public required DateTime CreatedAt { get; set; }
-   public required DateTime UpdatedAt { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required DateTime HireDate { get; set; }
+    public required Boolean IsActive { get; set; } = true;
+    public required DateTime CreatedAt { get; set; }
+    public required DateTime UpdatedAt { get; set; }
 
 }
+
---
+++ server/src/Infrastructure/Identity/OmnipulseIdentityDbContextFactory.cs
@@ -0,0 +1,18 @@
+using System;
+using Identity.DbContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity;
+
+public class OmnipulseIdentityDbContextFactory
+{
+    public IdentityDbContext createDbContext(string[] args)
+    { 
+        var optionsBuilder = new DbContextOptionsBuilder<IdentityDbContext>();
+
+        // TODO: add db url
+        optionsBuilder.UseSqlServer("");
+
+        return new IdentityDbContext(optionsBuilder.Options); 
+    }
+}
