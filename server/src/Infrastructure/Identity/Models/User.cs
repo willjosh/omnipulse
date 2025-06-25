3973ddc5512cf6d06e454b643c0d93be8f043f59
Dicko Evaldo <z5451386@ad.unsw.edu.au>
2025-06-25 14:22:21 +1000

    feat: add User model

---
+++ server/src/Infrastructure/Identity/Models/User.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace Identity.Models;
+
+public class User : IdentityUser
+{
+   public required string FirstName { get; set; } 
+   public required string LastName { get; set; } 
+   public required DateTime HireDate { get; set; }
+   public required Boolean IsActive { get; set; } = true;
+   public required DateTime CreatedAt { get; set; }
+   public required DateTime UpdatedAt { get; set; }
+
+}
