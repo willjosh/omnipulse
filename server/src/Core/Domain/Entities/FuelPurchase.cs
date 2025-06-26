d6c4a061dc174175325bbfa38ad9e645f976b465
Annie Le <z5477709@ad.unsw.edu.au>
2025-06-26 13:29:02 +1000

    refactor: Removed inherited fields: Id, UpdatedAt, CreatedAt

--- server/src/Core/Domain/Entities/FuelPurchase.cs
+++ server/src/Core/Domain/Entities/FuelPurchase.cs
@@ -2,29 +2,24 @@ namespace Domain.Entities;
 
 public class FuelPurchase : BaseEntity
 {
-    public required int Id { get; set; }
-    public DateTime? Created_at { get; set; }
+    public required int VehicleId { get; set; }
 
-    public DateTime? Updated_at { get; set; }
+    public required int PurchasedBy { get; set; }
 
-    public required int Vehicle_id { get; set; }
+    public required DateTime PurchaseDate { get; set; }
 
-    public required int Purchased_by { get; set; }
-
-    public required DateTime Purchase_date { get; set; }
-
-    public required double Odometer_reading { get; set; }
+    public required double OdometerReading { get; set; }
 
     public required double Volume { get; set; }
 
-    public required float Price_per_unit { get; set; }
+    public required double PricePerUnit { get; set; }
 
-    public required float Total_cost { get; set; }
+    public required float TotalCost { get; set; }
 
-    public required string Fuel_station { get; set; }
+    public required string FuelStation { get; set; }
 
-    public required string Receipt_number { get; set; }
+    public required string ReceiptNumber { get; set; }
 
-    public string? Text { get; set; } 
+    public string? Text { get; set; }
 
 }
