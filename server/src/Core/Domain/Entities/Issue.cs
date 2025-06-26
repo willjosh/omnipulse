2062bf88ba6a90477b184db4613ffe296b29fd25
Dicko Evaldo <z5451386@ad.unsw.edu.au>
2025-06-26 14:20:42 +1000

    fix(lint): additional formmating

--- server/src/Core/Domain/Entities/Issue.cs
+++ server/src/Core/Domain/Entities/Issue.cs
@@ -16,7 +16,7 @@ public class Issue : BaseEntity
     public string? ResolutionNotes { get; set; }
 
     // Navigation Properties
-    public required ICollection<IssueAttachment> IssueAttachment { get; set; } = [];
+    public required ICollection<IssueAttachment> IssueAttachments { get; set; } = [];
     public required ICollection<WorkOrderIssue> WorkOrderIssue { get; set; } = [];
     public required Vehicle Vehicle { get; set; }
 
