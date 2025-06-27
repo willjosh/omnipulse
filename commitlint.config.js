25bceade34fdb7aae289f5ed9620cab2bce41559
SAW <99191294+nicsaw@users.noreply.github.com>
2025-06-27 21:06:31 +1000

    W18ADATE-199 feat: set up git commit linter in husky

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,9 @@
+// https://commitlint.js.org
+//
+// Commit message format:
+// <Jira work item key> <type>: <subject>
+// Example: W18ADATE-123 feat: add login form
+
 const JIRA_PROJECT_KEY = 'W18ADATE';
 const COMMIT_TYPES = [
   'feat',
@@ -10,18 +16,18 @@ const COMMIT_TYPES = [
   'chore',
 ];
 
-// https://commitlint.js.org
-// <Jira work item key> <type>: <message>
 module.exports = {
-  rules: {
-    'header-pattern': [
-      2,
-      'always',
-      new RegExp(
-        `^${JIRA_PROJECT_KEY}-\\d+\\s(${COMMIT_TYPES.join('|')}):\\s.{1,}$`
+  parserPreset: {
+    parserOpts: {
+      headerPattern: new RegExp(
+        `^${JIRA_PROJECT_KEY}-\\d+\\s(${COMMIT_TYPES.join('|')}):\\s(.+)$`
       ),
-    ],
+      headerCorrespondence: ['type', 'subject'],
+    },
+  },
+  rules: {
+    'type-enum': [2, 'always', COMMIT_TYPES],
+    'type-empty': [2, 'never'],
+    'subject-empty': [2, 'never'],
   },
 };
-
-// test commit messages linter
