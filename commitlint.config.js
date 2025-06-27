d1c1e487f393677e1f5178ddaee071181a3cb333
SAW <99191294+nicsaw@users.noreply.github.com>
2025-06-27 20:39:38 +1000

    feat: add git commit message linter -> <Jira work item key> <type>: <message>

---
+++ commitlint.config.js
@@ -0,0 +1,25 @@
+const JIRA_PROJECT_KEY = 'W18ADATE';
+const COMMIT_TYPES = [
+  'feat',
+  'fix',
+  'refactor',
+  'test',
+  'ops',
+  'style',
+  'docs',
+  'chore',
+];
+
+// https://commitlint.js.org
+// <Jira work item key> <type>: <message>
+module.exports = {
+  rules: {
+    'header-pattern': [
+      2,
+      'always',
+      new RegExp(
+        `^${JIRA_PROJECT_KEY}-\\d+\\s(${COMMIT_TYPES.join('|')}):\\s.{1,}$`
+      ),
+    ],
+  },
+};
