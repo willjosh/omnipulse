bfeb676fa23c8e3590fdf595a1aa82725082ada6
Dicko Evaldo <z5451386@ad.unsw.edu.au>
2025-06-28 21:59:05 +1000

    W18ADATE-206 feat: add DbSet for every entities

--- server/src/Infrastructure/Persistence/DatabaseContext/OmnipulseDatabaseContext.cs
+++ server/src/Infrastructure/Persistence/DatabaseContext/OmnipulseDatabaseContext.cs
@@ -7,6 +7,31 @@ namespace Persistence.DatabaseContext;
 
 public class OmnipulseDatabaseContext(DbContextOptions<OmnipulseDatabaseContext> options) : IdentityDbContext<User>(options)
 {
+    public DbSet<CheckListItem> ChecklistItems { get; set; }
+    public DbSet<FuelPurchase> FuelPurchases { get; set; }
+    public DbSet<InspectionAttachment> InspectionAttachments { get; set; }
+    public DbSet<InspectionChecklistResponse> InspectionChecklistResponses { get; set; }
+    public DbSet<InspectionType> InspectionTypes { get; set; }
+    public DbSet<Invoice> Invoices { get; set; }
+    public DbSet<IssueAttachment> IssueAttachments { get; set; }
+    public DbSet<Issue> Issues { get; set; }
+    public DbSet<MaintenanceHistory> MaintenanceHistories { get; set; }
+    public DbSet<ServiceProgram> ServicePrograms { get; set; }
+    public DbSet<ServiceReminder> ServiceReminders { get; set; }
+    public DbSet<ServiceSchedule> ServiceSchedules { get; set; }
+    public DbSet<ServiceScheduleTask> ServiceScheduleTasks { get; set; }
+    public DbSet<ServiceTask> ServiceTasks { get; set; }
+    public DbSet<VehicleAlert> VehicleAlerts { get; set; }
+    public DbSet<Vehicle> Vehicles { get; set; }
+    public DbSet<VehicleDocument> VehicleDocuments { get; set; }
+    public DbSet<VehicleGroup> VehicleGroups { get; set; }
+    public DbSet<VehicleImage> VehicleImages { get; set; }
+    public DbSet<VehicleInspection> VehicleInspections { get; set; }
+    public DbSet<VehicleServiceProgram> VehicleServicePrograms { get; set; }
+    public DbSet<WorkOrder> WorkOrders { get; set; }
+    public DbSet<WorkOrderIssue> WorkOrderIssues { get; set; }
+    public DbSet<WorkOrderLineItem> WorkOrderLineItems { get; set; }
+
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
