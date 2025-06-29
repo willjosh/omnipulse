c9d2cc156959fe1638fce88adfe77221a8dd9557
Dicko Evaldo <z5451386@ad.unsw.edu.au>
2025-06-29 17:46:57 +1000

    W18ADATE-193 feat: add logger and mapper to the CreateVehicleCommandHandler

--- server/src/Core/Application/Features/Vehicles/Command/CreateVehicle/CreateVehicleCommandHandler.cs
+++ server/src/Core/Application/Features/Vehicles/Command/CreateVehicle/CreateVehicleCommandHandler.cs
@@ -1,6 +1,7 @@
-using System;
+using Application.Contracts.Logger;
 using Application.Contracts.Persistence;
 using Application.Features.Vehicle.Command.CreateVehicle;
+using AutoMapper;
 using MediatR;
 
 namespace Application.Features.Vehicles.Command.CreateVehicle;
@@ -8,10 +9,14 @@ namespace Application.Features.Vehicles.Command.CreateVehicle;
 public class CreateVehicleCommandHandler : IRequestHandler<CreateVehicleCommand, int>
 {
     private readonly IVehicleRepository _vehicleRepository;
+    private readonly IMapper _mapper;
+    private readonly IAppLogger<CreateVehicleCommandHandler> _logger;
 
-    public CreateVehicleCommandHandler(IVehicleRepository VehicleRepository)
+    public CreateVehicleCommandHandler(IVehicleRepository VehicleRepository, IMapper Mapper, IAppLogger<CreateVehicleCommandHandler> Logger)
     {
         _vehicleRepository = VehicleRepository;
+        _mapper = Mapper;
+        _logger = Logger;
     }
 
     public Task<int> Handle(CreateVehicleCommand request, CancellationToken cancellationToken)
