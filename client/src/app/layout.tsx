ba34f48ec8621e0a6a6e65a1451a98a9ff1a28b7
William Josh Limantara <w.limantara@student.unsw.edu.au>
2025-06-30 23:25:59 +1000

    W18ADATE-253 style: remove extra padding from root layout content container

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
         <NavBar />
         <div className="flex min-h-screen">
           <SideBar />
-          <div className="flex-1 p-4 overflow-auto">{children}</div>
+          <div className="flex-1 overflow-auto">{children}</div>
         </div>
       </body>
     </html>
