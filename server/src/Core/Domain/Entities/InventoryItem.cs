1516da4abe03d481e14d1e9da0388b442ca94432
SAW <99191294+nicsaw@users.noreply.github.com>
2025-07-01 01:43:11 +1000

    W18ADATE-286 feat: Update InventoryItem entity to include more fields

--- server/src/Core/Domain/Entities/InventoryItem.cs
+++ server/src/Core/Domain/Entities/InventoryItem.cs
@@ -7,16 +7,19 @@ public class InventoryItem : BaseEntity
 {
     public required string ItemNumber { get; set; }
     public required string ItemName { get; set; }
-    public required string Description { get; set; }
-    public required InventoryItemCategoryEnum Category { get; set; }
-    public required string Brand { get; set; }
-    public required string Supplier { get; set; }
-    public decimal UnitPrice { get; set; }
-    public double WeightKG { get; set; }
+    public string? Description { get; set; }
+    public InventoryItemCategoryEnum? Category { get; set; }
+    public string? Manufacturer { get; set; }
+    public string? ManufacturerPartNumber { get; set; }
+    public string? UniversalProductCode { get; set; } // https://en.wikipedia.org/wiki/Universal_Product_Code We use UPC-A (12 digits)
+    public decimal? UnitCost { get; set; }
+    public InventoryItemUnitCostMeasurementUnitEnum? UnitCostMeasurementUnit { get; set; }
+    public string? Supplier { get; set; }
+    public double? WeightKG { get; set; }
     public required Boolean IsActive { get; set; } = true;
-    public required string CompatibleVehicleTypes { get; set; }
+    public string? CompatibleVehicleTypes { get; set; }
 
-    // navigation properties 
+    // Navigation Properties
     public required ICollection<Inventory> Inventories { get; set; } = [];
     public required ICollection<WorkOrderLineItem> WorkOrderLineItems { get; set; } = [];
 }
