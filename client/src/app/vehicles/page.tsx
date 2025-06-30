0141b6374cb656f024ba4f3e9cfc7409c1de3ca6
Michael Elijah Natawidjaja <natawidjajamichael@gmail.com>
2025-07-01 00:55:50 +1000

    W18ADATE-276 feat: add vehicles list page

--- client/src/app/vehicles/page.tsx
+++ client/src/app/vehicles/page.tsx
@@ -1,7 +1,13 @@
+"use client";
 import React from "react";
+import VehicleList from "../features/vehicle/components/list/VehicleList";
 
 const Vehicles = () => {
-  return <div>Vehicles</div>;
+  return (
+    <div className="flex justify-center">
+      <VehicleList />
+    </div>
+  );
 };
 
 export default Vehicles;
