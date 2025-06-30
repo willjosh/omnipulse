15e6f0c497f86e4c8d3ab6aab2c2cdfe3dfded04
Dicko Evaldo <z5451386@ad.unsw.edu.au>
2025-07-01 01:25:34 +1000

    W18ADATE-287 feat: add Pagination Models

---
+++ server/src/Core/Application/Models/PaginationModels/PaginationParameters.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Application.Models;
+
+public class PaginationParameters
+{
+    private const int MaxPageSize = 100;
+    private int _pageSize = 10;
+
+    public int PageNumber { get; set; } = 1;
+    
+    public int PageSize
+    {
+        get => _pageSize;
+        set => _pageSize = value > MaxPageSize ? MaxPageSize : value;
+    }
+
+    public string? Search { get; set; }
+    public string? SortBy { get; set; }
+    public bool SortDescending { get; set; } = false;
+}
