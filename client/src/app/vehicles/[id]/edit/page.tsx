25b56c6ec3468b4b59f55bd78627ab4127393293
Michael Elijah Natawidjaja <natawidjajamichael@gmail.com>
2025-07-01 21:18:58 +1000

    W18ADATE-214 feat: Add edit page to update vehicle

---
+++ client/src/app/vehicles/[id]/edit/page.tsx
@@ -0,0 +1,7 @@
+import VehicleFormContainer from "@/app/_features/vehicle/components/forms/VehicleFormContainer";
+
+const EditVehiclePage = () => {
+  return <VehicleFormContainer mode="edit" />;
+};
+
+export default EditVehiclePage;
