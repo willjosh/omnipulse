e2b08ed875ae6b1f1284d069d6da210247e18aee
Dicko Evaldo <z5451386@ad.unsw.edu.au>
2025-07-01 19:49:12 +1000

    W18ADATE-295 feat: add paramateres for CreateTechnicianCommand

--- server/src/Core/Application/Features/Users/Command/CreateTechnician/CreateTechnicianCommand.cs
+++ server/src/Core/Application/Features/Users/Command/CreateTechnician/CreateTechnicianCommand.cs
@@ -2,4 +2,9 @@ using MediatR;
 
 namespace Application.Features.Users.Command.CreateTechnician;
 
-public record CreateTechnicianCommand : IRequest<Guid> { }
+public record CreateTechnicianCommand(
+   string FirstName,
+   string LastName,
+   DateTime HireDate,
+   bool IsActive = true
+) : IRequest<Guid> { }
