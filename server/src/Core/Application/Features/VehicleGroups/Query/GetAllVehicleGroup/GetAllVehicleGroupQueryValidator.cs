33417754998edca36b84cb5be479023e25921855
Clarence Muljadi <clarencemuljadi@gmail.com>
2025-07-01 15:55:27 +1000

    W18ADATE-291 feat: created GetAllVehicleGroupQueryValidator

---
+++ server/src/Core/Application/Features/VehicleGroups/Query/GetAllVehicleGroup/GetAllVehicleGroupQueryValidator.cs
@@ -0,0 +1,24 @@
+using System;
+using Application.Features.Shared;
+using Domain.Entities;
+using FluentValidation;
+
+namespace Application.Features.VehicleGroups.Query.GetAllVehicleGroup;
+
+public class GetAllVehicleGroupQueryValidator : AbstractValidator<GetAllVehicleGroupQuery>
+{
+    public GetAllVehicleGroupQueryValidator()
+    {
+        var vehicleGroupSortFields = new[] {
+            "name",
+            "description",
+            "isactive",
+            "createdat",
+            "updatedat"
+        };
+
+        RuleFor(x => x.Parameters)
+            .NotNull()
+            .SetValidator(new PaginationValidator<VehicleGroup>(vehicleGroupSortFields));
+    }
+}
