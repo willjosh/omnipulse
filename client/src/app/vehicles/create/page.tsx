65ecae2c3d826e46fd093134c95c2a33b44fe2de
Michael Elijah Natawidjaja <natawidjajamichael@gmail.com>
2025-07-01 21:19:40 +1000

    W18ADATE-214 refactor: rename vehicle create form component and route for clarity

---
+++ client/src/app/vehicles/create/page.tsx
@@ -0,0 +1,7 @@
+import VehicleFormContainer from "@/app/_features/vehicle/components/forms/VehicleFormContainer";
+
+const CreateVehiclePage = () => {
+  return <VehicleFormContainer mode="create" />;
+};
+
+export default CreateVehiclePage;
