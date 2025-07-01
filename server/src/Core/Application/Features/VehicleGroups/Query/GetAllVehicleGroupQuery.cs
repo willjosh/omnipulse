fb576699c204ca71cb815a351f0dd03e6787649e
Clarence Muljadi <clarencemuljadi@gmail.com>
2025-07-01 15:49:58 +1000

    W18ADATE-291 feat: created GetAllVehicleGroupQuery

---
+++ server/src/Core/Application/Features/VehicleGroups/Query/GetAllVehicleGroupQuery.cs
@@ -0,0 +1,7 @@
+using Application.Models;
+using Application.Models.PaginationModels;
+using MediatR;
+
+namespace Application.Features.VehicleGroups.Query.GetAllVehicleGroup;
+
+public record GetAllVehicleGroupQuery(PaginationParameters Parameters) : IRequest<PagedResult<GetAllVehicleGroupDTO>> { }
