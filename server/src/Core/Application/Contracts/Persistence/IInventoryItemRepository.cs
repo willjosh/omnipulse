54c076a8a249a648e0df505cfe653592e6cc78f5
SAW <99191294+nicsaw@users.noreply.github.com>
2025-07-01 14:37:47 +1000

    W18ADATE-286 feat: add IInventoryItemRepository

---
+++ server/src/Core/Application/Contracts/Persistence/IInventoryItemRepository.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Domain.Entities.Enums;
+
+namespace Application.Contracts.Persistence;
+
+public interface IInventoryItemRepository : IGenericRepository<InventoryItem>
+{
+    // Uniqueness checks
+    Task<bool> IsItemNumberUniqueAsync(string itemNumber);
+    Task<bool> IsUniversalProductCodeUniqueAsync(string? universalProductCode);
+    Task<bool> IsManufacturerPartNumberUniqueAsync(string? manufacturer, string? manufacturerPartNumber);
+}
