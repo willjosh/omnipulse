253c5e49d9e8a4d9024741b45a4b498f2ea576d8
Dicko Evaldo <z5451386@ad.unsw.edu.au>
2025-07-01 20:02:34 +1000

    W18ADATE-295 feat: add UserMappingProfile for CreateTechnicianCommand

---
+++ server/src/Core/Application/MappingProfiles/UserMappingProfile.cs
@@ -0,0 +1,21 @@
+using System;
+using Application.Features.Users.Command.CreateTechnician;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.MappingProfiles;
+
+public class UserMappingProfile : Profile
+{
+    public UserMappingProfile()
+    {
+        CreateMap<CreateTechnicianCommand, User>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore()) 
+            .ForMember(dest => dest.MaintenanceHistories, opt => opt.Ignore())
+            .ForMember(dest => dest.IssueAttachments, opt => opt.Ignore())
+            .ForMember(dest => dest.VehicleAssignments, opt => opt.Ignore())
+            .ForMember(dest => dest.VehicleDocuments, opt => opt.Ignore())
+            .ForMember(dest => dest.VehicleInspections, opt => opt.Ignore())
+            .ForMember(dest => dest.Vehicles, opt => opt.Ignore());
+    }
+}
