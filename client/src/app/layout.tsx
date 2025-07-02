d88f83dfc3bf4f2f07dc04b54c7d4b331ad8814d
William Josh Limantara <w.limantara@student.unsw.edu.au>
2025-07-02 17:32:55 +1000

    W18ADATE-255 fix: improve responsive layout and fix broken work order details

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
         <NavBar />
         <div className="flex w-screen max-w-screen">
           <SideBar />
-          <div className="flex-1 w-[70%] mt-4">{children}</div>
+          <div className="flex-1 sm:w-[70%] md:w-[60%] lg:w-[50%] mt-4">
+            {children}
+          </div>
         </div>
       </body>
     </html>
