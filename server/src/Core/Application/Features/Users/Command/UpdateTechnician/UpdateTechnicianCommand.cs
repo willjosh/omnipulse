d603d54fa4196767796dfe17ed0435b298717953
Dicko Evaldo <z5451386@ad.unsw.edu.au>
2025-07-04 18:17:31 +1000

    W18ADATE-303 refactor: made updateTechnicianCommand nullable

--- server/src/Core/Application/Features/Users/Command/UpdateTechnician/UpdateTechnicianCommand.cs
+++ server/src/Core/Application/Features/Users/Command/UpdateTechnician/UpdateTechnicianCommand.cs
@@ -6,10 +6,9 @@ namespace Application.Features.Users.Command.UpdateTechnician;
 
 public record UpdateTechnicianCommand(
     string Id,
-    string Email,
-    string FirstName,
-    string LastName,
-    DateTime HireDate,
-    bool IsActive = true
+    string? FirstName,
+    string? LastName,
+    DateTime? HireDate,
+    bool? IsActive 
 ) : IRequest<string>
 { }
