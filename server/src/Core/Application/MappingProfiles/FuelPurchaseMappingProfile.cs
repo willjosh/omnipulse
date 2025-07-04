3b293525c9dcc9bd92386fb955b40f61bcdd3ab2
SAW <99191294+nicsaw@users.noreply.github.com>
2025-07-04 17:36:12 +1000

    W18ADATE-304 fix: add navigation properties to FuelPurchaseMappingProfile

--- server/src/Core/Application/MappingProfiles/FuelPurchaseMappingProfile.cs
+++ server/src/Core/Application/MappingProfiles/FuelPurchaseMappingProfile.cs
@@ -1,6 +1,9 @@
 using System;
+
 using Application.Features.FuelLogging.Command.CreateFuelPurchase;
+
 using AutoMapper;
+
 using Domain.Entities;
 
 namespace Application.MappingProfiles;
@@ -12,6 +15,8 @@ public class FuelPurchaseMappingProfile : Profile
         CreateMap<CreateFuelPurchaseCommand, FuelPurchase>()
             .ForMember(dest => dest.ID, opt => opt.Ignore()) // Don't map ID - will be auto-generated
             .ForMember(dest => dest.CreatedAt, opt => opt.Ignore()) // Handled by BaseEntity
-            .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore()); // Handled by BaseEntity
+            .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore()) // Handled by BaseEntity
+            .ForMember(dest => dest.Vehicle, opt => opt.Ignore()) // Navigation property
+            .ForMember(dest => dest.User, opt => opt.Ignore()); // Navigation property
     }
 }
