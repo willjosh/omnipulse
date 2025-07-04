d68cdd85917aa14d4f8d9cc54734cd062b6b2167
SAW <99191294+nicsaw@users.noreply.github.com>
2025-07-04 12:17:09 +1000

    W18ADATE-304 style: FuelPurchase entity

--- server/src/Core/Domain/Entities/FuelPurchase.cs
+++ server/src/Core/Domain/Entities/FuelPurchase.cs
@@ -3,26 +3,17 @@ namespace Domain.Entities;
 public class FuelPurchase : BaseEntity
 {
     public required int VehicleId { get; set; }
-
     public required string PurchasedByUserId { get; set; }
-
     public required DateTime PurchaseDate { get; set; }
-
     public required double OdometerReading { get; set; }
-
     public required double Volume { get; set; }
-
     public required decimal PricePerUnit { get; set; }
-
     public required decimal TotalCost { get; set; }
-
     public required string FuelStation { get; set; }
-
     public required string ReceiptNumber { get; set; }
-
     public string? Notes { get; set; }
 
-    // navigation properties
+    // Navigation Properties
     public required Vehicle Vehicle { get; set; }
     public required User User { get; set; }
 }
