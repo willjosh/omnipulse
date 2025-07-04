2e5296ea91a54fde41c15d2e962834c9b9adf959
SAW <99191294+nicsaw@users.noreply.github.com>
2025-07-04 17:42:54 +1000

    W18ADATE-304 feat: add IsReceiptNumberUniqueAsync to FuelPurchasesRepository

--- server/src/Infrastructure/Persistence/Repository/FuelPurchasesRepository.cs
+++ server/src/Infrastructure/Persistence/Repository/FuelPurchasesRepository.cs
@@ -13,13 +13,15 @@ public class FuelPurchasesRepository : GenericRepository<FuelPurchase>, IFuelPur
 {
     public FuelPurchasesRepository(OmnipulseDatabaseContext context) : base(context) { }
 
-    public async Task<bool> IsValidOdometerReading(int vehicleId, double OdometerReading)
+    public async Task<bool> IsValidOdometerReading(int vehicleId, double odometerReading)
     {
         var vehicle = await _dbSet.FindAsync(vehicleId);
-        if (vehicle != null)
-        {
-            return OdometerReading >= vehicle.OdometerReading;
-        }
+        if (vehicle != null) return odometerReading >= vehicle.OdometerReading;
         return true;
+    }
+
+    public async Task<bool> IsReceiptNumberUniqueAsync(string receiptNumber)
+    {
+        return !await _dbSet.AnyAsync(fp => fp.ReceiptNumber == receiptNumber);
     }
 }
