f6495c3cf5340b59e62fa59900363f3bc920f562
SAW <99191294+nicsaw@users.noreply.github.com>
2025-07-06 15:49:22 +1000

    W18ADATE-312 feat: add IServiceTaskRepository

---
+++ server/src/Core/Application/Contracts/Persistence/IServiceTaskRepository.cs
@@ -0,0 +1,12 @@
+using Application.Models;
+using Application.Models.PaginationModels;
+
+using Domain.Entities;
+
+namespace Application.Contracts.Persistence;
+
+public interface IServiceTaskRepository : IGenericRepository<ServiceTask>
+{
+    public Task<bool> NameExistAsync(string name);
+    public Task<PagedResult<ServiceTask>> GetAllServiceTasksPagedAsync(PaginationParameters parameters);
+}
