945633a573efc006c570e965be498020d82d4bbc
SAW <99191294+nicsaw@users.noreply.github.com>
2025-07-07 08:08:32 +1000

    W18ADATE-318 feat: ServiceTaskMappingProfile - add UpdateServiceTask

--- server/src/Core/Application/MappingProfiles/ServiceTaskMappingProfile.cs
+++ server/src/Core/Application/MappingProfiles/ServiceTaskMappingProfile.cs
@@ -1,4 +1,5 @@
 using Application.Features.ServiceTasks.Command.CreateServiceTask;
+using Application.Features.ServiceTasks.Command.UpdateServiceTask;
 
 using AutoMapper;
 
@@ -14,6 +15,14 @@ public class ServiceTaskMappingProfile : Profile
             .ForMember(dest => dest.ID, opt => opt.Ignore()) // Don't map ID - will be auto-generated
             .ForMember(dest => dest.CreatedAt, opt => opt.Ignore()) // Handled by BaseEntity
             .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore()) // Handled by BaseEntity
+            .ForMember(dest => dest.ServiceScheduleTasks, opt => opt.Ignore()) // Navigation Collection
+            .ForMember(dest => dest.MaintenanceHistories, opt => opt.Ignore()) // Navigation Collection
+            .ForMember(dest => dest.WorkOrderLineItems, opt => opt.Ignore()); // Navigation Collection
+
+        CreateMap<UpdateServiceTaskCommand, ServiceTask>()
+            .ForMember(dest => dest.ID, opt => opt.Ignore()) // Don't update ID
+            .ForMember(dest => dest.CreatedAt, opt => opt.Ignore()) // Don't update CreatedAt
+            .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore()) // Will be handled by BaseEntity/Context
             .ForMember(dest => dest.ServiceScheduleTasks, opt => opt.Ignore()) // Navigation Collection
             .ForMember(dest => dest.MaintenanceHistories, opt => opt.Ignore()) // Navigation Collection
             .ForMember(dest => dest.WorkOrderLineItems, opt => opt.Ignore()); // Navigation Collection
