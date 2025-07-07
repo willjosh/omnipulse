93f5d0566e522e57ce8e0bd426dce2727e40f96f
SAW <99191294+nicsaw@users.noreply.github.com>
2025-07-08 01:58:11 +1000

    W18ADATE-330 test: test new pre-commit script lints staged files

--- server/tests/Application.Test/FuelPurchases/CommandTest/CreateFuelPurchases/CreateFuelPurchaseCommandHandlerTest.cs
+++ server/tests/Application.Test/FuelPurchases/CommandTest/CreateFuelPurchases/CreateFuelPurchaseCommandHandlerTest.cs
@@ -4,6 +4,10 @@ using Application.Contracts.Logger;
 using Application.Contracts.Persistence;
 using Application.Exceptions;
 using Application.Features.FuelLogging.Command.CreateFuelPurchase;
+
+
+
+
 using Application.MappingProfiles;
 
 
