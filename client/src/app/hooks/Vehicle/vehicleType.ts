f1810460d9fabdec2945a0d91d164e818f44985c
William Josh Limantara <w.limantara@student.unsw.edu.au>
2025-07-07 15:58:45 +1000

    W18ADATE-308 refactor: move enum types to separate vehicleEnum file

--- client/src/app/hooks/Vehicle/vehicleType.ts
+++ client/src/app/hooks/Vehicle/vehicleType.ts
@@ -1,3 +1,9 @@
+import {
+  VehicleTypeEnum,
+  FuelTypeEnum,
+  VehicleStatusEnum,
+} from "./vehicleEnum";
+
 export interface Vehicle {
   id: number;
   Name: string;
@@ -87,34 +93,4 @@ export interface VehicleFilter {
   sortBy?: string;
   sortOrder?: "asc" | "desc";
   search?: string;
-}
-
-export enum VehicleTypeEnum {
-  TRUCK = 1,
-  VAN = 2,
-  CAR = 3,
-  MOTORCYCLE = 4,
-  BUS = 5,
-  HEAVY_VEHICLE = 6,
-  TRAILER = 7,
-  OTHER = 8,
-}
-
-export enum FuelTypeEnum {
-  PETROL = 1,
-  DIESEL = 2,
-  ELECTRIC = 3,
-  HYBRID = 4,
-  GAS = 5,
-  LPG = 6,
-  CNG = 7,
-  BIO_DIESEL = 8,
-  OTHER = 9,
-}
-
-export enum VehicleStatusEnum {
-  ACTIVE = 1,
-  MAINTENANCE = 2,
-  OUT_OF_SERVICE = 3,
-  INACTIVE = 4,
 }
