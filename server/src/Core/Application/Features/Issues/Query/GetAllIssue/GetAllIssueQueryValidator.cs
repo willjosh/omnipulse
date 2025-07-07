07ae13067eb95d06b25697bed33aef5901006952
Clarence Muljadi <clarencemuljadi@gmail.com>
2025-07-07 17:52:49 +1000

    W18ADATE-328 feat: created GetAllIssueQueryValidator

---
+++ server/src/Core/Application/Features/Issues/Query/GetAllIssue/GetAllIssueQueryValidator.cs
@@ -0,0 +1,30 @@
+using System;
+
+using Application.Features.Shared;
+
+using Domain.Entities;
+
+using FluentValidation;
+
+namespace Application.Features.Issues.Query.GetAllIssue;
+
+public class GetAllIssueQueryValidator : AbstractValidator<GetAllIssueQuery>
+{
+    public GetAllIssueQueryValidator()
+    {
+        var issueSortFields = new[] {
+            "title",
+            "status",
+            "prioritylevel",
+            "category",
+            "reporteddate",
+            "resolveddate",
+            "createdat",
+            "updatedat"
+        };
+
+        RuleFor(x => x.Parameters)
+            .NotNull()
+            .SetValidator(new PaginationValidator<Issue>(issueSortFields));
+    }
+}
