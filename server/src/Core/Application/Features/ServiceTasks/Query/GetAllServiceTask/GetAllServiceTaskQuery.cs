70066a0437de690f101953d85d92dad93fae23d3
SAW <99191294+nicsaw@users.noreply.github.com>
2025-07-07 16:15:02 +1000

    W18ADATE-324 feat: add GetAllServiceTaskQuery

---
+++ server/src/Core/Application/Features/ServiceTasks/Query/GetAllServiceTask/GetAllServiceTaskQuery.cs
@@ -0,0 +1,7 @@
+using Application.Models;
+using Application.Models.PaginationModels;
+using MediatR;
+
+namespace Application.Features.ServiceTasks.Query.GetAllServiceTask;
+
+public record GetAllServiceTaskQuery(PaginationParameters Parameters) : IRequest<PagedResult<GetAllServiceTaskDTO>> { }
