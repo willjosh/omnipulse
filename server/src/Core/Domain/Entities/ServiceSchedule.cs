d53e984a4bcbb842c1791064a6b057bae5c67a58
SAW <99191294+nicsaw@users.noreply.github.com>
2025-07-08 15:38:00 +1000

    W18ADATE-327 refactor: change ServiceSchedule entity properties

--- server/src/Core/Domain/Entities/ServiceSchedule.cs
+++ server/src/Core/Domain/Entities/ServiceSchedule.cs
@@ -1,16 +1,48 @@
 using System;
 
+using Domain.Entities.Enums;
+
 namespace Domain.Entities;
 
+/// <summary>
+/// Represents a service schedule that defines the cadence at which maintenance tasks should be performed for vehicles.
+/// </summary>
+/// <remarks>
+/// A service schedule can be configured by time, mileage, or a combination of both.
+/// It belongs to a single <see cref="ServiceProgram"/>.
+/// 1:N <see cref="ServiceScheduleTask"/>s.
+///
+/// <list type="bullet">
+///   <item>Must reference exactly one ServiceProgram via <see cref="ServiceProgramID"/>.</item>
+///   <item>At least one of the following recurrence options must be provided:
+///     <list type="bullet">
+///       <item>Time-based (<see cref="TimeIntervalValue"/> &amp; <see cref="TimeIntervalUnit"/>)</item>
+///       <item>Mileage-based (<see cref="MileageInterval"/>)</item>
+///     </list>
+///   </item>
+///   <item>Buffer values (<see cref="TimeBufferValue"/>, <see cref="MileageBuffer"/>) are optional. If provided, they cannot exceed their corresponding interval.</item>
+///   <item>First service properties (<see cref="FirstServiceTimeValue"/>, <see cref="FirstServiceTimeUnit"/>, <see cref="FirstServiceMileage"/>) are optional. If provided, they require their related interval fields to be set:
+///     <list type="bullet">
+///       <item><see cref="FirstServiceTimeValue"/> and <see cref="FirstServiceTimeUnit"/> require <see cref="TimeIntervalValue"/> and <see cref="TimeIntervalUnit"/> to be set.</item>
+///       <item><see cref="FirstServiceMileage"/> requires <see cref="MileageInterval"/> to be set.</item>
+///     </list>
+///   </item>
+///   <item>The schedule can be activated or deactivated using the <see cref="IsActive"/> flag. Inactive schedules are ignored when calculating upcoming service reminders.</item>
+/// </list>
+/// </remarks>
 public class ServiceSchedule : BaseEntity
 {
     public required int ServiceProgramID { get; set; }
     public required string Name { get; set; }
-    public required int IntervalMileage { get; set; }
-    public required int IntervalDays { get; set; }
-    public required int IntervalHours { get; set; }
-    public required int BufferMileage { get; set; }
-    public required int BufferDays { get; set; }
+    public int? TimeIntervalValue { get; set; }
+    public TimeUnitEnum? TimeIntervalUnit { get; set; }
+    public int? TimeBufferValue { get; set; }
+    public TimeUnitEnum? TimeBufferUnit { get; set; }
+    public int? MileageInterval { get; set; } // km
+    public int? MileageBuffer { get; set; } // km
+    public int? FirstServiceTimeValue { get; set; } // Requires TimeIntervalValue and TimeIntervalUnit if set
+    public TimeUnitEnum? FirstServiceTimeUnit { get; set; } // Requires TimeIntervalValue and TimeIntervalUnit if set
+    public int? FirstServiceMileage { get; set; } // Requires MileageInterval if set
     public required bool IsActive { get; set; } = true;
 
     // Navigation Properties
