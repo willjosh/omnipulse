af5317cad4fd51eb7f4ebe123d62b1ec9f5c2802
Clarence Muljadi <clarencemuljadi@gmail.com>
2025-07-09 18:14:23 +1000

    W18ADATE-328 feat: add mapping profile for GetAllMaintenanceHistoryDTO

--- server/src/Core/Application/MappingProfiles/MaintenanceHistoryMappingProfile.cs
+++ server/src/Core/Application/MappingProfiles/MaintenanceHistoryMappingProfile.cs
@@ -1,4 +1,5 @@
 using Application.Features.MaintenanceHistories.Command.CreateMaintenanceHistory;
+using Application.Features.MaintenanceHistories.Query.GetAllMaintenanceHistories;
 
 using AutoMapper;
 
@@ -17,5 +18,12 @@ public class MaintenanceHistoryMappingProfile : Profile
             .ForMember(dest => dest.ServiceTask, opt => opt.Ignore())
             .ForMember(dest => dest.User, opt => opt.Ignore())
             .ForMember(dest => dest.InventoryTransactions, opt => opt.Ignore());
+
+        CreateMap<MaintenanceHistory, GetAllMaintenanceHistoryDTO>()
+            .ForMember(dest => dest.MaintenanceHistoryID, opt => opt.MapFrom(src => src.ID))
+            .ForMember(dest => dest.VehicleName, opt => opt.MapFrom(src => src.Vehicle != null ? src.Vehicle.Name : string.Empty))
+            .ForMember(dest => dest.WorkOrderNumber, opt => opt.MapFrom(src => src.WorkOrder != null ? src.WorkOrder.Title : string.Empty))
+            .ForMember(dest => dest.ServiceTaskName, opt => opt.MapFrom(src => src.ServiceTask != null ? src.ServiceTask.Name : string.Empty))
+            .ForMember(dest => dest.TechnicianName, opt => opt.MapFrom(src => src.User != null ? src.User.FirstName + " " + src.User.LastName : string.Empty));
     }
 }
