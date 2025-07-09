b10f72e87163e99b6db0656da797a7dc504ab482
Clarence Muljadi <clarencemuljadi@gmail.com>
2025-07-09 21:39:28 +1000

    W18ADATE-343 feat: added GetIssueDetailsDTO

---
+++ server/src/Core/Application/Features/Issues/Query/GetIssueDetails/GetIssueDetailsDTO.cs
@@ -0,0 +1,25 @@
+using System;
+
+using Domain.Entities.Enums;
+
+namespace Application.Features.Issues.Query.GetIssueDetails;
+
+public class GetIssueDetailsDTO
+{
+    public int ID { get; set; }
+    public required int IssueNumber { get; set; }
+    public required string Title { get; set; }
+    public string? Description { get; set; }
+    public required IssueStatusEnum Status { get; set; }
+    public required PriorityLevelEnum PriorityLevel { get; set; }
+    public required IssueCategoryEnum Category { get; set; }
+    public required int VehicleID { get; set; }
+    public required string VehicleName { get; set; }
+    public required string ReportedByUserID { get; set; }
+    public required string ReportedByUserName { get; set; }
+    public DateTime? ReportedDate { get; set; }
+    public DateTime? ResolvedDate { get; set; }
+    public string? ResolutionNotes { get; set; }
+    public string? ResolvedByUserID { get; set; }
+    public string? ResolvedByUserName { get; set; }
+}
