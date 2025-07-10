273ccec7f733f7048ed536820289719bc28646ef
SAW <99191294+nicsaw@users.noreply.github.com>
2025-07-11 00:23:40 +1000

    W18ADATE-348 feat: stub GetAllServiceSchedulePagedAsync()

--- server/src/Infrastructure/Persistence/Repository/ServiceScheduleRepository.cs
+++ server/src/Infrastructure/Persistence/Repository/ServiceScheduleRepository.cs
@@ -1,4 +1,6 @@
 using Application.Contracts.Persistence;
+using Application.Models;
+using Application.Models.PaginationModels;
 
 using Domain.Entities;
 
@@ -9,4 +11,9 @@ namespace Persistence.Repository;
 public class ServiceScheduleRepository : GenericRepository<ServiceSchedule>, IServiceScheduleRepository
 {
     public ServiceScheduleRepository(OmnipulseDatabaseContext context) : base(context) { }
+
+    public Task<PagedResult<ServiceSchedule>> GetAllServiceSchedulePagedAsync(PaginationParameters parameters)
+    {
+        throw new NotImplementedException();
+    }
 }
