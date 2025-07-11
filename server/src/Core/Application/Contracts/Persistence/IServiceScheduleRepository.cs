fcc9586336808ff5425445b23334b3420b0b0764
SAW <99191294+nicsaw@users.noreply.github.com>
2025-07-11 17:12:52 +1000

    W18ADATE-348 docs: add comments to IServiceScheduleRepository

--- server/src/Core/Application/Contracts/Persistence/IServiceScheduleRepository.cs
+++ server/src/Core/Application/Contracts/Persistence/IServiceScheduleRepository.cs
@@ -7,5 +7,10 @@ namespace Application.Contracts.Persistence;
 
 public interface IServiceScheduleRepository : IGenericRepository<ServiceSchedule>
 {
-    Task<PagedResult<ServiceSchedule>> GetAllServiceSchedulePagedAsync(PaginationParameters parameters);
+    /// <summary>
+    /// Retrieves all service schedules in the system.
+    /// </summary>
+    /// <param name="parameters">Pagination parameters.</param>
+    /// <returns>A <see cref="PagedResult{ServiceSchedule}"/> containing the paged list of service schedules.</returns>
+    Task<PagedResult<ServiceSchedule>> GetAllServiceSchedulesPagedAsync(PaginationParameters parameters);
 }
