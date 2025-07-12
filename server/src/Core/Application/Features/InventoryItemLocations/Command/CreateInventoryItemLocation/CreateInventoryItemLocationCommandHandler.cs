187f72cfc7321ddaa05f58ff729223671fb57488
Clarence Muljadi <clarencemuljadi@gmail.com>
2025-07-12 21:03:58 +1000

    W18ADATE-349 feat: implemented command handler for create inventory item location

--- server/src/Core/Application/Features/InventoryItemLocations/Command/CreateInventoryItemLocation/CreateInventoryItemLocationCommandHandler.cs
+++ server/src/Core/Application/Features/InventoryItemLocations/Command/CreateInventoryItemLocation/CreateInventoryItemLocationCommandHandler.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Linq;
 
 using Application.Contracts.Logger;
 using Application.Contracts.Persistence;
+using Application.Exceptions;
 
 using AutoMapper;
 
@@ -20,8 +22,35 @@ public class CreateInventoryItemLocationCommandHandler : IRequestHandler<CreateI
     private readonly IAppLogger<CreateInventoryItemLocationCommandHandler> _logger;
     private readonly IValidator<CreateInventoryItemLocationCommand> _validator;
 
-    public Task<int> Handle(CreateInventoryItemLocationCommand request, CancellationToken cancellationToken)
+    public CreateInventoryItemLocationCommandHandler(IInventoryItemLocationRepository inventoryItemLocationRepository, IMapper mapper, IAppLogger<CreateInventoryItemLocationCommandHandler> logger, IValidator<CreateInventoryItemLocationCommand> validator)
     {
-        throw new NotImplementedException();
+        _inventoryItemLocationRepository = inventoryItemLocationRepository;
+        _mapper = mapper;
+        _logger = logger;
+        _validator = validator;
+    }
+
+    public async Task<int> Handle(CreateInventoryItemLocationCommand request, CancellationToken cancellationToken)
+    {
+        // validate request
+        var validationResult = await _validator.ValidateAsync(request, cancellationToken);
+        if (!validationResult.IsValid)
+        {
+            var errorMessages = string.Join("; ", validationResult.Errors.Select(e => e.ErrorMessage));
+            _logger.LogWarning($"CreateInventoryItemLocationCommand - Validation failed: {errorMessages}");
+            throw new BadRequestException(errorMessages);
+        }
+
+        // map request to inventory item location domain entity
+        var inventoryItemLocation = _mapper.Map<InventoryItemLocation>(request);
+
+        // add new inventory item location
+        var newInventoryItemLocation = await _inventoryItemLocationRepository.AddAsync(inventoryItemLocation);
+
+        // save changes
+        await _inventoryItemLocationRepository.SaveChangesAsync();
+
+        // return inventory item location ID
+        return newInventoryItemLocation.ID;
     }
 }
