2b71e65d7c3973a25b74beb8b0d55ccf7f587614
Michael Elijah Natawidjaja <natawidjajamichael@gmail.com>
2025-07-14 19:29:24 +1000

    W18ADATE-236 feat: add technician list page

---
+++ client/src/app/contacts/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TechnicianList from "../_features/technician/components/TechnicianList";
+
+const ContactsPage = () => {
+  return (
+    <div className="flex justify-center h-screen overflow-hidden">
+      <TechnicianList />
+    </div>
+  );
+};
+
+export default ContactsPage;
