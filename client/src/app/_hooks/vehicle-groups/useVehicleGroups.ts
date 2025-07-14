94df0610ac427dfa79fbf9923c584ff002044a24
Michael Elijah Natawidjaja <natawidjajamichael@gmail.com>
2025-07-14 13:19:46 +1000

    W18ADATE-229 feat: add react query to update vehicle group

--- client/src/app/_hooks/vehicle-groups/useVehicleGroups.ts
+++ client/src/app/_hooks/vehicle-groups/useVehicleGroups.ts
@@ -1,6 +1,10 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { agent } from "@/app/_lib/axios/agent";
-import { VehicleGroup, CreateVehicleGroupCommand } from "./vehicleGroupTypes";
+import {
+  VehicleGroup,
+  CreateVehicleGroupCommand,
+  UpdateVehicleGroupCommand,
+} from "./vehicleGroupTypes";
 
 export const useVehicleGroups = () => {
   const queryClient = useQueryClient();
@@ -36,10 +40,25 @@ export const useVehicleGroups = () => {
     },
   });
 
+  // Update vehicle group mutation
+  const updateVehicleGroupMutation = useMutation({
+    mutationFn: async (updateVehicle: UpdateVehicleGroupCommand) => {
+      const response = await agent.put(
+        `vehicleGroups/${updateVehicle.id}`,
+        updateVehicle,
+      );
+      return response.data;
+    },
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ["vehicleGroups"] });
+    },
+  });
+
   return {
     vehicleGroups,
     isLoading,
     createVehicleGroupMutation,
     deleteVehicleGroupMutation,
+    updateVehicleGroupMutation,
   };
 };
