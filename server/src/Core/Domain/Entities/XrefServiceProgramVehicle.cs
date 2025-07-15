aaa359a95edb35738b67fd3a3e45c5b982b17b05
SAW <99191294+nicsaw@users.noreply.github.com>
2025-07-15 20:14:41 +1000

    W18ADATE-364 refactor: refactor XrefServiceProgramVehicle properties

--- server/src/Core/Domain/Entities/XrefServiceProgramVehicle.cs
+++ server/src/Core/Domain/Entities/XrefServiceProgramVehicle.cs
@@ -1,18 +1,13 @@
-using System;
-
 namespace Domain.Entities;
 
 public class XrefServiceProgramVehicle
 {
-    public required int VehicleID { get; set; }
     public required int ServiceProgramID { get; set; }
-    public required DateTime AssignedDate { get; set; }
-    public required bool IsActive { get; set; }
+    public required int VehicleID { get; set; }
 
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 
-    // navigation properties
-    public required Vehicle Vehicle { get; set; }
+    // Navigation Properties
     public required ServiceProgram ServiceProgram { get; set; }
+    public required Vehicle Vehicle { get; set; }
 }
