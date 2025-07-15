28dcdfa03c046c844dadc805da216c3b672a46fe
Clarence Muljadi <clarencemuljadi@gmail.com>
2025-07-15 13:58:56 +1000

    W18ADATE-359 refactor: removed pagination for getting all inventory item locations

--- server/src/Core/Application/Features/InventoryItemLocations/Query/GetAllInventoryItemLocation/GetAllInventoryItemLocationQuery.cs
+++ server/src/Core/Application/Features/InventoryItemLocations/Query/GetAllInventoryItemLocation/GetAllInventoryItemLocationQuery.cs
@@ -5,4 +5,4 @@ using MediatR;
 
 namespace Application.Features.InventoryItemLocations.Query.GetAllInventoryItemLocation;
 
-public record GetAllInventoryItemLocationQuery(PaginationParameters Parameters) : IRequest<PagedResult<GetAllInventoryItemLocationDTO>>;
+public record GetAllInventoryItemLocationQuery() : IRequest<List<GetAllInventoryItemLocationDTO>>;
--- server/src/Core/Application/Features/InventoryItemLocations/Query/GetAllInventoryItemLocation/GetAllInventoryItemLocationQueryHandler.cs
+++ server/src/Core/Application/Features/InventoryItemLocations/Query/GetAllInventoryItemLocation/GetAllInventoryItemLocationQueryHandler.cs
@@ -1,14 +1,37 @@
-using Application.Models;
-using Application.Models.PaginationModels;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Application.Contracts.Logger;
+using Application.Contracts.Persistence;
+
+using AutoMapper;
 
 using MediatR;
 
 namespace Application.Features.InventoryItemLocations.Query.GetAllInventoryItemLocation;
 
-public class GetAllInventoryItemLocationQueryHandler : IRequestHandler<GetAllInventoryItemLocationQuery, PagedResult<GetAllInventoryItemLocationDTO>>
+public class GetAllInventoryItemLocationQueryHandler : IRequestHandler<GetAllInventoryItemLocationQuery, List<GetAllInventoryItemLocationDTO>>
 {
-    public Task<PagedResult<GetAllInventoryItemLocationDTO>> Handle(GetAllInventoryItemLocationQuery request, CancellationToken cancellationToken)
+    private readonly IInventoryItemLocationRepository _repository;
+    private readonly IMapper _mapper;
+    private readonly IAppLogger<GetAllInventoryItemLocationQueryHandler> _logger;
+
+    public GetAllInventoryItemLocationQueryHandler(
+        IInventoryItemLocationRepository repository,
+        IMapper mapper,
+        IAppLogger<GetAllInventoryItemLocationQueryHandler> logger)
     {
-        throw new NotImplementedException();
+        _repository = repository;
+        _mapper = mapper;
+        _logger = logger;
+    }
+
+    public async Task<List<GetAllInventoryItemLocationDTO>> Handle(GetAllInventoryItemLocationQuery request, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Handling GetAllInventoryItemLocationQuery (no pagination)");
+        var allEntities = await _repository.GetAllAsync();
+        var allDTOs = _mapper.Map<List<GetAllInventoryItemLocationDTO>>(allEntities);
+        return allDTOs;
     }
 }
