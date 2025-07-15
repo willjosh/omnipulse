ee093362bd4c57a5efb6c5efc080a9f9eff15757
SAW <99191294+nicsaw@users.noreply.github.com>
2025-07-15 18:58:53 +1000

    W18ADATE-364 feat: add AddVehicleToServiceProgramCommand

---
+++ server/src/Core/Application/Features/ServicePrograms/Command/AddVehicleToServiceProgram/AddVehicleToServiceProgramCommand.cs
@@ -0,0 +1,16 @@
+using Domain.Entities;
+
+using MediatR;
+
+namespace Application.Features.ServicePrograms.Command.AddVehicleToServiceProgram;
+
+/// <summary>
+/// Command for adding an existing <see cref="Vehicle"/> to an existing <see cref="ServiceProgram"/>.
+/// </summary>
+/// <param name="ServiceProgramID">The ID of the existing Service Program.</param>
+/// <param name="VehicleID">The ID of the existing Vehicle to be added.</param>
+/// <returns>A tuple containing the ID of the <see cref="ServiceProgram"/> and the ID of the <see cref="Vehicle"/>.</returns>
+public record AddVehicleToServiceProgramCommand(
+    int ServiceProgramID,
+    int VehicleID
+) : IRequest<(int ServiceProgramID, int VehicleID)>;
