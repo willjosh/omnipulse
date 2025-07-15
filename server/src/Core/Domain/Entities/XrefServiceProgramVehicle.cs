40f85e58b12277ec80620efa6fe137c5764f8431
SAW <99191294+nicsaw@users.noreply.github.com>
2025-07-15 20:23:35 +1000

    W18ADATE-364 refactor: add AddedByUserID to XrefServiceProgramVehicle

--- server/src/Core/Domain/Entities/XrefServiceProgramVehicle.cs
+++ server/src/Core/Domain/Entities/XrefServiceProgramVehicle.cs
@@ -2,12 +2,15 @@ namespace Domain.Entities;
 
 public class XrefServiceProgramVehicle
 {
+    // Composite PK
     public required int ServiceProgramID { get; set; }
     public required int VehicleID { get; set; }
 
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public required string AddedByUserID { get; set; } // FK
+    public required DateTime AddedAt { get; set; } = DateTime.UtcNow;
 
     // Navigation Properties
     public required ServiceProgram ServiceProgram { get; set; }
     public required Vehicle Vehicle { get; set; }
+    public required User User { get; set; }
 }
