fcb4be3e7bc57e9b75c27cf09afa6b4d753d3c9c
William Josh Limantara <w.limantara@student.unsw.edu.au>
2025-07-16 23:25:05 +1000

    W18ADATE-368 feat: make Name column sortable

--- client/src/app/_features/service-task/components/ServiceTaskTableColumns.ts
+++ client/src/app/_features/service-task/components/ServiceTaskTableColumns.ts
@@ -1,5 +1,5 @@
 export const serviceTaskTableColumns = [
-  { key: "Name", header: "Name", width: "250px" },
+  { key: "Name", header: "Name", width: "250px", sortable: true },
   { key: "Description", header: "Description", width: "320px" },
   { key: "EstimatedLabourHours", header: "Est. Labour Hours", width: "200px" },
   { key: "EstimatedCost", header: "Est. Cost" },
