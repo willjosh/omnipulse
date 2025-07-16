61bd50ac9681d4ccccbf6c528c630d598081669e
William Josh Limantara <w.limantara@student.unsw.edu.au>
2025-07-16 17:21:36 +1000

    W18ADATE-366 fix: make DataTable stretch to full width when fewer columns

--- client/src/app/service-tasks/page.tsx
+++ client/src/app/service-tasks/page.tsx
@@ -118,6 +118,7 @@ export default function ServiceTaskListPage() {
         showActions={false}
         emptyState={emptyState}
         onRowClick={handleRowClick}
+        fixedLayout={false}
       />
     </div>
   );
