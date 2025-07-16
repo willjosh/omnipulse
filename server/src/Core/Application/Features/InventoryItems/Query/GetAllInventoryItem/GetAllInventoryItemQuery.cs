f6ebcb127af65484d5179ea0e45cb565ccf5f455
Clarence Muljadi <clarencemuljadi@gmail.com>
2025-07-16 22:37:40 +1000

    W18ADATE-360 feat: created GetAllInventoryItemQuery

---
+++ server/src/Core/Application/Features/InventoryItems/Query/GetAllInventoryItem/GetAllInventoryItemQuery.cs
@@ -0,0 +1,8 @@
+using Application.Models;
+using Application.Models.PaginationModels;
+
+using MediatR;
+
+namespace Application.Features.InventoryItems.Query.GetAllInventoryItem;
+
+public record GetAllInventoryItemQuery(PaginationParameters Parameters) : IRequest<PagedResult<GetAllInventoryItemDTO>>;
