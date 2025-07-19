4d3d66a92705ccbc0425aa6c6c47cf5288a670be
SAW <99191294+nicsaw@users.noreply.github.com>
2025-07-20 00:38:38 +1000

    W18ADATE-375 refactor: make ValidSortFields class-level field

--- server/src/Core/Application/Features/ServicePrograms/Query/GetAllServiceProgramVehicle/GetAllServiceProgramVehicleQueryValidator.cs
+++ server/src/Core/Application/Features/ServicePrograms/Query/GetAllServiceProgramVehicle/GetAllServiceProgramVehicleQueryValidator.cs
@@ -8,20 +8,20 @@ namespace Application.Features.ServicePrograms.Query.GetAllServiceProgramVehicle
 
 public class GetAllServiceProgramVehicleQueryValidator : AbstractValidator<GetAllServiceProgramVehicleQuery>
 {
+    private static readonly string[] ValidSortFields = [
+        "vehiclename",
+        "addedat",
+    ];
+
     public GetAllServiceProgramVehicleQueryValidator()
     {
         RuleFor(x => x.ServiceProgramID)
             .GreaterThan(0)
             .WithMessage($"{nameof(GetAllServiceProgramVehicleQuery.ServiceProgramID)} must be greater than 0.");
 
-        var validSortFields = new[] {
-            "vehiclename",
-            "addedat",
-        };
-
         RuleFor(x => x.Parameters)
             .NotNull()
             .WithMessage($"{nameof(GetAllServiceProgramVehicleQuery.Parameters)} cannot be null.")
-            .SetValidator(new PaginationValidator<Vehicle>(validSortFields));
+            .SetValidator(new PaginationValidator<Vehicle>(ValidSortFields));
     }
 }
