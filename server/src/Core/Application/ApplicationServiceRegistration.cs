7b01354c1fcd2928a5ae35f723fbc0136dcd5770
SAW <99191294+nicsaw@users.noreply.github.com>
2025-07-21 16:10:44 +1000

    W18ADATE-376 feat: add ApplicationServiceRegistration for DI container setup

---
+++ server/src/Core/Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,29 @@
+using System.Reflection;
+
+using FluentValidation;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
+
+public static class ApplicationServiceRegistration
+{
+    /// <summary>
+    /// Adds Application services to the DI container.
+    /// </summary>
+    /// <param name="services">The service collection to register services with.</param>
+    /// <returns>The service collection with the services added.</returns>
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        // Register AutoMapper - all mapping profiles
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+        // Register MediatR - all handlers
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+        // Register FluentValidation - all validators
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+        return services;
+    }
+}
