f8a6493c4d532da52af7b2de41398123305be6a1
SAW <99191294+nicsaw@users.noreply.github.com>
2025-07-21 21:42:23 +1000

    W18ADATE-375 feat: ServiceProgramsController - add GetServiceProgramVehicles endpoint

--- server/src/Api/Controllers/ServiceProgramsController.cs
+++ server/src/Api/Controllers/ServiceProgramsController.cs
@@ -4,7 +4,9 @@ using Application.Features.ServicePrograms.Command.DeleteServiceProgram;
 using Application.Features.ServicePrograms.Command.RemoveVehicleFromServiceProgram;
 using Application.Features.ServicePrograms.Command.UpdateServiceProgram;
 using Application.Features.ServicePrograms.Query;
+using Application.Features.ServicePrograms.Query.GetAllServiceProgramVehicle;
 using Application.Features.ServicePrograms.Query.GetServiceProgram;
+using Application.Models.PaginationModels;
 
 using Domain.Entities;
 
@@ -24,6 +26,7 @@ namespace Api.Controllers;
 /// <item><b>POST /api/serviceprograms</b> - <see cref="CreateServiceProgram"/> - <see cref="CreateServiceProgramCommand"/></item>
 /// <item><b>PUT /api/serviceprograms/{id}</b> - <see cref="UpdateServiceProgram"/> - <see cref="UpdateServiceProgramCommand"/></item>
 /// <item><b>DELETE /api/serviceprograms/{id}</b> - <see cref="DeleteServiceProgram"/> - <see cref="DeleteServiceProgramCommand"/></item>
+/// <item><b>GET /api/serviceprograms/{serviceProgramId}/vehicles</b> - <see cref="GetServiceProgramVehicles"/> - <see cref="GetAllServiceProgramVehicleQuery"/></item>
 /// <item><b>POST /api/serviceprograms/{id}/vehicles</b> - <see cref="AddVehicleToServiceProgram"/> - <see cref="AddVehicleToServiceProgramCommand"/></item>
 /// <item><b>DELETE /api/serviceprograms/{id}/vehicles/{vehicleId}</b> - <see cref="RemoveVehicleFromServiceProgram"/> - <see cref="RemoveVehicleFromServiceProgramCommand"/></item>
 /// </list>
@@ -184,9 +187,46 @@ public sealed class ServiceProgramsController : ControllerBase
     }
 
     /// <summary>
+    /// Retrieves a paginated list of all vehicles assigned to a specific service program.
+    /// </summary>
+    /// <param name="serviceProgramId">The ID of the service program.</param>
+    /// <param name="parameters">Pagination parameters.</param>
+    /// <param name="cancellationToken">Cancellation token for the operation.</param>
+    /// <returns>A paginated result containing vehicles assigned to the service program.</returns>
+    /// <response code="200">Returns the paginated list of assigned vehicles.</response>
+    /// <response code="400">Pagination parameters are invalid.</response>
+    /// <response code="404">Service program not found.</response>
+    /// <response code="500">Internal server error occurred.</response>
+    [HttpGet("{serviceProgramId:int}/vehicles")]
+    [ProducesResponseType(typeof(PagedResult<XrefServiceProgramVehicleDTO>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult<PagedResult<XrefServiceProgramVehicleDTO>>> GetServiceProgramVehicles(
+        int serviceProgramId,
+        [FromQuery] PaginationParameters parameters,
+        CancellationToken cancellationToken)
+    {
+        try
+        {
+            _logger.LogInformation($"{nameof(GetServiceProgramVehicles)}() - Called");
+
+            var query = new GetAllServiceProgramVehicleQuery(serviceProgramId, parameters);
+            var result = await _mediator.Send(query, cancellationToken);
+
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, $"{nameof(GetServiceProgramVehicles)}() - ERROR");
+            return StatusCode(StatusCodes.Status500InternalServerError, new { message = "An error occurred while retrieving assigned vehicles for the service program." });
+        }
+    }
+
+    /// <summary>
     /// Adds a vehicle to a service program.
     /// </summary>
-    /// <param name="id">The ID of the service program.</param>
+    /// <param name="serviceProgramId">The ID of the service program.</param>
     /// <param name="command">The command containing the vehicle ID to add to the service program.</param>
     /// <param name="cancellationToken">Cancellation token for the operation.</param>
     /// <returns>A tuple containing the service program ID and vehicle ID.</returns>
@@ -195,14 +235,14 @@ public sealed class ServiceProgramsController : ControllerBase
     /// <response code="404">Service program or vehicle not found.</response>
     /// <response code="409">Vehicle is already assigned to this service program.</response>
     /// <response code="500">Internal server error occurred.</response>
-    [HttpPost("{id:int}/vehicles")]
+    [HttpPost("{serviceProgramId:int}/vehicles")]
     [ProducesResponseType(typeof((int ServiceProgramID, int VehicleID)), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     [ProducesResponseType(StatusCodes.Status409Conflict)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<ActionResult<(int ServiceProgramID, int VehicleID)>> AddVehicleToServiceProgram(
-        int id,
+        int serviceProgramId,
         [FromBody] AddVehicleToServiceProgramCommand command,
         CancellationToken cancellationToken)
     {
@@ -210,9 +250,9 @@ public sealed class ServiceProgramsController : ControllerBase
         {
             _logger.LogInformation($"{nameof(AddVehicleToServiceProgram)}() - Called");
 
-            if (id != command.ServiceProgramID) return ValidationProblem($"{nameof(AddVehicleToServiceProgram)} - Route ID and body ID mismatch.");
+            if (serviceProgramId != command.ServiceProgramID) return ValidationProblem($"{nameof(AddVehicleToServiceProgram)} - Route ID and body ID mismatch.");
 
-            var result = await _mediator.Send(command with { ServiceProgramID = id }, cancellationToken);
+            var result = await _mediator.Send(command with { ServiceProgramID = serviceProgramId }, cancellationToken);
 
             return Ok(result);
         }
