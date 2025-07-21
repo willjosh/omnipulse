20682541328fd8c438c2e9250b387951e484820e
SAW <99191294+nicsaw@users.noreply.github.com>
2025-07-21 12:49:51 +1000

    W18ADATE-376 feat: set up Swagger UI

--- server/src/Api/Program.cs
+++ server/src/Api/Program.cs
@@ -1,21 +1,57 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+// Add and configure Swagger middleware
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Omnipulse API",
+        Version = "v1",
+        Description = "A comprehensive fleet management system API.",
+        Contact = new OpenApiContact
+        {
+            Name = "OmniDevs",
+            Email = "Nicholassaw2004@gmail.com"
+        }
+    });
+
+    // Include XML comments
+    var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
+
+// Add CORS to allow frontend requests
+builder.Services.AddCors(
+    options => options.AddPolicy("AllowFrontend",
+        policy => policy.WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+    )
+);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Omnipulse API");
+        c.RoutePrefix = "swagger";
+    });
 }
 
 app.UseHttpsRedirection();
 
+app.UseCors("AllowFrontend");
+
+// app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
