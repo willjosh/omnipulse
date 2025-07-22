eb425238f438d7f3a32e4c9bc0a9f6eab620d5ee
SAW <99191294+nicsaw@users.noreply.github.com>
2025-07-22 14:16:58 +1000

    W18ADATE-375 feat: enhance DTOs with example annotations for swagger

--- server/src/Core/Application/Features/ServiceTasks/Query/ServiceTaskDTO.cs
+++ server/src/Core/Application/Features/ServiceTasks/Query/ServiceTaskDTO.cs
@@ -4,11 +4,24 @@ namespace Application.Features.ServiceTasks.Query;
 
 public class ServiceTaskDTO
 {
+    /// <example>1</example>
     public int ID { get; set; }
+
+    /// <example>Service Task Name</example>
     public required string Name { get; set; }
+
+    /// <example>Service Task Description</example>
     public string? Description { get; set; }
+
+    /// <example>2.5</example>
     public required double EstimatedLabourHours { get; set; }
+
+    /// <example>100.00</example>
     public required decimal EstimatedCost { get; set; }
+
+    /// <example>1</example>
     public required ServiceTaskCategoryEnum Category { get; set; }
+
+    /// <example>true</example>
     public required bool IsActive { get; set; }
 }
