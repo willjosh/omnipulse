6714e0cecbb94e450117b55796155f78e8786e2e
William Josh Limantara <w.limantara@student.unsw.edu.au>
2025-07-24 14:43:14 +1000

    W18ADATE-395 chore: update axios agent baseURL to match backend API

--- client/src/app/_lib/axios/agent.ts
+++ client/src/app/_lib/axios/agent.ts
@@ -6,7 +6,7 @@ const sleep = (delay: number) => {
   });
 };
 
-export const agent = axios.create({ baseURL: "http://localhost:3000" });
+export const agent = axios.create({ baseURL: "http://localhost:5100" });
 
 agent.interceptors.response.use(async response => {
   try {
