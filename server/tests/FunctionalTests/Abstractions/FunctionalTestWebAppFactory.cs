45c65df2f8f6ba9b3d62f7c686bba293d23a4141
SAW <99191294+nicsaw@users.noreply.github.com>
2025-07-27 22:38:18 +1000

    W18ADATE-423 feat: add FunctionalTestWebAppFactory

---
+++ server/tests/FunctionalTests/Abstractions/FunctionalTestWebAppFactory.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+using Persistence.DatabaseContext;
+
+using Testcontainers.MsSql;
+
+namespace FunctionalTests.Abstractions;
+
+public class FunctionalTestWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private const string MsSqlImage = "mcr.microsoft.com/mssql/server:2022-latest";
+    private const int MsSqlPort = 1433;
+    private const string MsSqlPassword = "YourStrong!Passw0rd";
+
+    private readonly MsSqlContainer _dbContainer = new MsSqlBuilder()
+        .WithImage(MsSqlImage)
+        .WithPortBinding(MsSqlPort)
+        .WithPassword(MsSqlPassword)
+        .Build();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            services.RemoveAll<DbContextOptions<OmnipulseDatabaseContext>>();
+
+            services.AddDbContext<OmnipulseDatabaseContext>(options =>
+                options.UseSqlServer(_dbContainer.GetConnectionString()));
+        });
+    }
+
+    public Task InitializeAsync() => _dbContainer.StartAsync();
+    public new Task DisposeAsync() => _dbContainer.StopAsync();
+}
