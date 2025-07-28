f7e33b601a2be356ba3dc38be4feb770a881ed90
SAW <99191294+nicsaw@users.noreply.github.com>
2025-07-29 01:22:38 +1000

    W18ADATE-432 test: add CreateUserIntegrationTests

---
+++ server/tests/IntegrationTests/Users/CreateUserIntegrationTests.cs
@@ -0,0 +1,42 @@
+using Application.Features.Users.Command.CreateTechnician;
+
+using Domain.Entities;
+
+using FluentAssertions;
+
+using IntegrationTests.Abstractions;
+
+namespace IntegrationTests.Users;
+
+[Trait("Entity", "User")]
+public class CreateUserIntegrationTests : BaseIntegrationTest
+{
+    public CreateUserIntegrationTests(IntegrationTestWebAppFactory factory) : base(factory) { }
+
+    [Fact]
+    public async Task Should_AddUserToDatabase_When_CommandIsValid()
+    {
+        // Arrange
+        var createCommand = new CreateTechnicianCommand(
+            Email: "technician@omnipulse.com",
+            Password: Faker.Internet.Password(length: 50, regexPattern: @"[a-zA-Z0-9!@#$%^&*()_+=\-]"),
+            FirstName: Faker.Name.FirstName(),
+            LastName: Faker.Name.LastName(),
+            HireDate: DateTime.UtcNow.AddYears(-1),
+            IsActive: true
+        );
+
+        // Act
+        Guid createdTechnicianGuid = await Sender.Send(createCommand);
+
+        // Assert
+        User? createdTechnicianEntity = await DbContext.Users.FindAsync(createdTechnicianGuid.ToString());
+        createdTechnicianEntity.Should().NotBeNull();
+
+        createdTechnicianEntity.Email.Should().Be(createCommand.Email);
+        createdTechnicianEntity.FirstName.Should().Be(createCommand.FirstName);
+        createdTechnicianEntity.LastName.Should().Be(createCommand.LastName);
+        createdTechnicianEntity.HireDate.Should().Be(createCommand.HireDate);
+        createdTechnicianEntity.IsActive.Should().Be(createCommand.IsActive);
+    }
+}
