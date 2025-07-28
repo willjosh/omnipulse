8df8f1b70b46eb2704eee5f6ff4116d7952df344
SAW <99191294+nicsaw@users.noreply.github.com>
2025-07-28 20:12:24 +1000

    W18ADATE-432 fix: Reference Api project in IntegrationTests project
    Fixed error:
    Can't find '/Users/nicholassaw/github/capstone-project-25t2-3900-w18a-date/server/tests/IntegrationTests/bin/Debug/net9.0/testhost.deps.json'. This file is required for functional tests to run properly. Ensure it's in your source project’s bin folder, or set `<PreserveCompilationContext>true</PreserveCompilationContext>` in your project file. Functional tests must either run from the build output folder or have this file copied there. A common issue is shadow copying being enabled when tests run.

--- server/tests/IntegrationTests/Abstractions/IntegrationTestWebAppFactory.cs
+++ server/tests/IntegrationTests/Abstractions/IntegrationTestWebAppFactory.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.TestHost;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.VisualStudio.TestPlatform.TestHost;
 
 using Persistence.DatabaseContext;
 
