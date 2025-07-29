3cec7fac6f01cba498f49d2f74ef18b353267d33
SAW <99191294+nicsaw@users.noreply.github.com>
2025-07-29 21:50:29 +1000

    W18ADATE-434 feat: WorkOrdersController - add GetWorkOrderInvoicePdf

--- server/src/Api/Controllers/WorkOrdersController.cs
+++ server/src/Api/Controllers/WorkOrdersController.cs
@@ -1,5 +1,6 @@
 using Application.Features.WorkOrders.Command.CreateWorkOrder;
 using Application.Features.WorkOrders.Query.GetWorkOrderDetail;
+using Application.Features.WorkOrders.Query.GetWorkOrderInvoicePdf;
 
 using Domain.Entities;
 
@@ -17,12 +18,12 @@ namespace Api.Controllers;
 /// <list type="bullet">
 /// <item><b>GET /api/workorders/{id}</b> - <see cref="GetWorkOrder"/> - <see cref="GetWorkOrderDetailQuery"/></item>
 /// <item><b>POST /api/workorders</b> - <see cref="CreateWorkOrder"/> - <see cref="CreateWorkOrderCommand"/></item>
+/// <item><b>GET /api/workorders/{id}/invoice.pdf</b> - <see cref="GetWorkOrderInvoicePdf"/> - <see cref="GetWorkOrderInvoicePdfQuery"/></item>
 /// </list>
 /// </remarks>
 [ApiController]
 [Route("api/[controller]")]
 [Consumes("application/json")]
-[Produces("application/json")]
 public sealed class WorkOrdersController : ControllerBase
 {
     private readonly IMediator _mediator;
@@ -92,6 +93,42 @@ public sealed class WorkOrdersController : ControllerBase
             var workOrderId = await _mediator.Send(command, cancellationToken);
 
             return CreatedAtAction(nameof(GetWorkOrder), new { id = workOrderId }, workOrderId);
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+
+    /// <summary>
+    /// Generates a PDF invoice for a specific work order.
+    /// </summary>
+    /// <param name="id">The ID of the work order to generate an invoice for.</param>
+    /// <param name="cancellationToken">Cancellation token for the operation.</param>
+    /// <returns>PDF invoice as a file download.</returns>
+    /// <response code="200">Returns the PDF invoice.</response>
+    /// <response code="404">Work order not found.</response>
+    /// <response code="500">Internal server error occurred.</response>
+    [HttpGet("{id:int}/invoice.pdf")]
+    [Produces("application/pdf")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> GetWorkOrderInvoicePdf(
+        int id,
+        CancellationToken cancellationToken)
+    {
+        try
+        {
+            _logger.LogInformation($"{nameof(GetWorkOrderInvoicePdf)}() - Called");
+
+            // Get PDF bytes directly from query
+            var query = new GetWorkOrderInvoicePdfQuery(id);
+            var pdfBytes = await _mediator.Send(query, cancellationToken);
+
+            // Return PDF as file download
+            var fileName = $"workorder{id}-invoice.pdf";
+            return File(pdfBytes, "application/pdf", fileName);
         }
         catch (Exception)
         {
