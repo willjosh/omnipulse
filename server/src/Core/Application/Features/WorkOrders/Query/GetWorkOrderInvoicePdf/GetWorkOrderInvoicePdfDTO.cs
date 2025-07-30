e97c8816e7bb8092ec9b58d4e36ac44231ddf9c1
SAW <99191294+nicsaw@users.noreply.github.com>
2025-07-30 10:35:12 +1000

    W18ADATE-434 feat: add GetWorkOrderInvoicePdfDTO

---
+++ server/src/Core/Application/Features/WorkOrders/Query/GetWorkOrderInvoicePdf/GetWorkOrderInvoicePdfDTO.cs
@@ -0,0 +1,24 @@
+namespace Application.Features.WorkOrders.Query.GetWorkOrderInvoicePdf;
+
+public class GetWorkOrderInvoicePdfDTO
+{
+    public required int WorkOrderID { get; set; }
+    public string? WorkOrderTitle { get; set; }
+    public string? WorkOrderDescription { get; set; }
+    public string? WorkOrderType { get; set; }
+    public string? WorkOrderPriorityLevel { get; set; }
+    public string? InvoiceNumber { get; set; }
+    public DateTime InvoiceDate { get; set; }
+    public string? VehicleName { get; set; }
+    public string? VehicleMake { get; set; }
+    public string? VehicleModel { get; set; }
+    public string? VehicleVIN { get; set; }
+    public string? VehicleLicensePlate { get; set; }
+    public string? AssignedToUserName { get; set; }
+    public DateTime? ScheduledStartDate { get; set; }
+    public DateTime? ActualStartDate { get; set; }
+    public List<WorkOrderLineItemInvoicePdfDTO> WorkOrderLineItems { get; set; } = [];
+    public decimal TotalLabourCost { get; set; }
+    public decimal TotalItemCost { get; set; }
+    public decimal TotalCost { get; set; }
+}
