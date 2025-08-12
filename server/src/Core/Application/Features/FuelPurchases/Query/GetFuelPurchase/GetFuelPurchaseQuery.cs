60b98a1594c6898902c928a9c709128b0ed82884
SAW <99191294+nicsaw@users.noreply.github.com>
2025-08-13 02:17:53 +1000

    W18ADATE-488 feat: add GetFuelPurchaseQuery

---
+++ server/src/Core/Application/Features/FuelPurchases/Query/GetFuelPurchase/GetFuelPurchaseQuery.cs
@@ -0,0 +1,14 @@
+using Application.Features.FuelPurchases.Query.GetAllFuelPurchases;
+
+using Domain.Entities;
+
+using MediatR;
+
+namespace Application.Features.FuelPurchases.Query.GetFuelPurchase;
+
+/// <summary>
+/// Query to get a single <see cref="FuelPurchase"/> by ID.
+/// </summary>
+/// <param name="FuelPurchaseID">The ID of the <see cref="FuelPurchase"/> to retrieve.</param>
+/// <returns>The <see cref="GetAllFuelPurchasesDTO"/> containing the <see cref="FuelPurchase"/> details.</returns>
+public sealed record GetFuelPurchaseQuery(int FuelPurchaseID) : IRequest<GetAllFuelPurchasesDTO>;
