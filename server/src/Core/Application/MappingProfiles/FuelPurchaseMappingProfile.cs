8bdf84ed7f301e90dde5c77e36b71d210142803b
SAW <99191294+nicsaw@users.noreply.github.com>
2025-08-12 22:31:13 +1000

    W18ADATE-486 feat: FuelPurchaseMappingProfile - add UpdateFuelPurchaseCommand -> FuelPurchase mapping

--- server/src/Core/Application/MappingProfiles/FuelPurchaseMappingProfile.cs
+++ server/src/Core/Application/MappingProfiles/FuelPurchaseMappingProfile.cs
@@ -1,4 +1,5 @@
 using Application.Features.FuelLogging.Command.CreateFuelPurchase;
+using Application.Features.FuelLogging.Command.UpdateFuelPurchase;
 using Application.Features.FuelPurchases.Query.GetAllFuelPurchases;
 
 using AutoMapper;
@@ -11,16 +12,22 @@ public class FuelPurchaseMappingProfile : Profile
 {
     public FuelPurchaseMappingProfile()
     {
-        CreateMap<CreateFuelPurchaseCommand, FuelPurchase>()
+        CreateMap<CreateFuelPurchaseCommand, FuelPurchase>(MemberList.Destination)
             .ForMember(dest => dest.ID, opt => opt.Ignore()) // Don't map ID - will be auto-generated
             .ForMember(dest => dest.CreatedAt, opt => opt.Ignore()) // Handled by BaseEntity
             .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore()) // Handled by BaseEntity
             .ForMember(dest => dest.Vehicle, opt => opt.Ignore()) // Navigation property
             .ForMember(dest => dest.User, opt => opt.Ignore()); // Navigation property
 
+        CreateMap<UpdateFuelPurchaseCommand, FuelPurchase>(MemberList.Destination)
+            .ForMember(dest => dest.ID, opt => opt.Ignore())
+            .ForMember(dest => dest.TotalCost, opt => opt.Ignore()) // Will be recalculated
+            .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+            .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore())
+            .ForMember(dest => dest.Vehicle, opt => opt.Ignore())
+            .ForMember(dest => dest.User, opt => opt.Ignore());
 
-
-        CreateMap<FuelPurchase, GetAllFuelPurchasesDTO>()
+        CreateMap<FuelPurchase, GetAllFuelPurchasesDTO>(MemberList.Destination)
             .ForMember(dest => dest.ID, opt => opt.MapFrom(src => src.ID))
             .ForMember(dest => dest.VehicleId, opt => opt.MapFrom(src => src.VehicleId))
             .ForMember(dest => dest.FuelStation, opt => opt.MapFrom(src => src.FuelStation))
